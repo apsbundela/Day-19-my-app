292c324faa4d42a5b9aaf68bb60172076009e6be
apsbundela <bundela.ayush99@gmail.com>
2022-01-05 21:39:43 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 export default function App() {
-  let id = 1;
-  let name = "ayush";
-  let list = ["amul", "ayush"];
-  let obj = {id :2 ,name :"amul"}
-
+  let list = [1, 2, 3, 34, 4, , 6, 7, 8, 9, 0, 2, 4, 65, 7];
   return (
     <div>
-      <h1>id : {id}</h1>
-      <h2>Mera NAAM :{name}</h2>
-      <h2>{list}</h2>
-      <h2>{obj.name}{obj.id}</h2>
-      <h2>i am {name} my id is {id}</h2>
+      {list.map(() => (
+        <Mycomponent></Mycomponent>
+      ))}
+      ;
+    </div>
+  );
+}
+
+function Mycomponent() {
+  let color = "bg-success ";
+  return (
+    <div className={color}>
+      <h2>Header</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nisi in
+        suscipit at rerum harum libero corporis blanditiis enim nulla.
+      </p>
     </div>
   );
 }
